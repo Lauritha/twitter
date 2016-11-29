cbf283a1cd9ab7b4137dc0c20126bea4ec2d2502
lauritha <lizamavergaralaura@gmail.com>
2016-11-29 00:02:35 -0300

    avances de estructura y metodos

--- app/app.js
+++ app/app.js
@@ -9,6 +9,65 @@ angular.module('miapp', ['ui.router'])
     $urlRouterProvider.otherwise('/')
 })
 angular.module('miapp')
-  .controller('ListController', function($scope) {
-    $scope.todos = [{id: 1, desc: 'Todo 1'}, {id: 2, desc: 'Todo 2'}]
+  .controller('ListController', function($scope, Tweet) {
+    $scope.Tweet = Tweet
+  })
+
+angular.module('miapp')
+  .service('Tweet', function Tweet() {
+    this.data = [{id: 1, desc: 'Mi primer tweet :)', favorito: false}, {id: 2, desc: 'Hola gente!', favorito: false}]
+    this.selected = undefined
+    this.new = '' 
+    this.temporal = ''
+
+    this.agregarTweet = function agregarTweet () {
+      this.data.push({
+        id: (new Date()).getTime(),
+        desc: this.new,
+        favorito: false,
+      })
+      this.new = ''
+    }
+
+    this.eliminarTweet = function eliminarTweet(id) {
+      this.data = this.data.filter(function filtrar(el) { 
+        return id !== el.id 
+      })
+    }
+
+    this.copiarTweet = function copiarTweet(id) {
+      this.data = this.data.filter(function filtrar(el) { 
+        return id !== el.id 
+      })
+    }
+
+    this.completarTweet = function completarTweet(id) {
+      this.data = this.data.map(function map(el) { 
+        if (el.id === id) {
+          el.favorito = !el.favorito
+        } 
+        return el 
+      })
+    }
+    this.seleccionarTweet = function seleccionarTweet(id) {
+      var servicio = this
+      this.data.map(function map(el) {
+        if (el.id === id) { 
+          servicio.temporal = el.desc
+        }
+      })
+      this.selected = id
+    }
+
+    this.actualizarTweet = function actualizarTweet() {
+      var servicio = this
+      this.data = this.data.map(function map(el) {
+        if (el.id === servicio.selected) {
+          el.desc = servicio.temporal
+        }
+        return el
+      })
+      this.selected = undefined
+    }
+
   })
